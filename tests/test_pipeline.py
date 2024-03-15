4a83dfe2174551c0fb2b6678a207c8041cffd284
Pedro Marques <pedromar2010@gmail.com>
2024-03-15 16:30:46 +0000

    Added Pytest to check function update_values()

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import pytest
-from pipeline import calculate_orders_per_product_per_week
+from pipeline import calculate_orders_per_product_per_week, update_values, main
 
 @pytest.fixture
 def sample_data():
@@ -25,3 +25,21 @@ def test_calculate_orders_per_product_per_week(sample_data):
     pd.testing.assert_frame_equal(result, expected_values, check_dtype=False)
 
 
+def test_update_values():
+    # Test case 1: Update all values
+    original_dict = {'a': 1, 'b': 2, 'c': 3}
+    update_dict = {'a': 10, 'b': 20, 'c': 30}
+    expected_result = {'a': 10, 'b': 20, 'c': 30}
+    assert update_values(original_dict, update_dict) == expected_result
+
+    # Test case 2: Update some values
+    original_dict = {'a': 1, 'b': 2, 'c': 3}
+    update_dict = {'b': 20, 'c': None}
+    expected_result = {'a': 1, 'b': 20, 'c': 3}
+    assert update_values(original_dict, update_dict) == expected_result
+
+    # Test case 3: Update no values
+    original_dict = {'a': 1, 'b': 2, 'c': 3}
+    update_dict = {}
+    expected_result = {'a': 1, 'b': 2, 'c': 3}
+    assert update_values(original_dict, update_dict) == expected_result
