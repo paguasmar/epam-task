df1f979bde94444dcd1475b5676c8d2e1b7eff9f
Pedro Marques <pedromar2010@gmail.com>
2024-03-16 19:13:32 +0000

    Refactor code formatting in pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -32,8 +32,9 @@ def setup_logging(log_file_path: str, log_level: str) -> None:
     console_handler.setLevel(log_level)
 
     # Create formatter and add it to the handlers
-    formatter: logging.Formatter = \
-        logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    formatter: logging.Formatter = logging.Formatter(
+        "%(asctime)s - %(levelname)s - %(message)s"
+    )
     file_handler.setFormatter(formatter)
     console_handler.setFormatter(formatter)
 
@@ -64,8 +65,9 @@ def load_config(config_file: str) -> Dict[str, Any]:
         return
 
 
-def update_values(original_dict: Dict[str, Any],
-                  update_dict: Dict[str, Any]) -> Dict[str, Any]:
+def update_values(
+    original_dict: Dict[str, Any], update_dict: Dict[str, Any]
+) -> Dict[str, Any]:
     """
     Update values in the original dictionary with values
     from the update dictionary.
@@ -84,7 +86,8 @@ def update_values(original_dict: Dict[str, Any],
 
 
 def calculate_orders_per_product_per_week(
-        df_products_sales: DataFrame) -> DataFrame:
+    df_products_sales: DataFrame,
+) -> DataFrame:
     """
     Calculate the number of orders per product per week.
 
@@ -99,8 +102,11 @@ def calculate_orders_per_product_per_week(
     df_copy: DataFrame = df_products_sales.copy(deep=False)
     df_copy["week"] = df_copy["order_purchase_timestamp"].dt.isocalendar().week
     df_copy["year"] = df_copy["order_purchase_timestamp"].dt.year
-    return df_copy.groupby(["product_id", "year",
-                            "week"]).size().reset_index(name="sales")
+    return (
+        df_copy.groupby(["product_id", "year", "week"])
+        .size()
+        .reset_index(name="sales")
+    )
 
 
 def main(args: argparse.Namespace) -> None:
@@ -130,11 +136,13 @@ def main(args: argparse.Namespace) -> None:
     logger.info("Reading input data...")
     try:
         df_orders: DataFrame = pd.read_csv(
-            config['orders_dataset_path'],
+            config["orders_dataset_path"],
             index_col="order_id",
-            parse_dates=["order_purchase_timestamp"])
+            parse_dates=["order_purchase_timestamp"],
+        )
         df_order_items: DataFrame = pd.read_csv(
-            config['order_items_dataset_path'])
+            config["order_items_dataset_path"]
+        )
     except FileNotFoundError as e:
         logger.error("Input data file not found: %s", str(e))
         return
@@ -144,10 +152,12 @@ def main(args: argparse.Namespace) -> None:
         return
 
     # Filter df_orders by order_status
-    logger.info("Filtering orders by status: %s",
-                config['order_status_filter'])
-    df_orders_delivered: DataFrame = \
-        df_orders[df_orders["order_status"] == config['order_status_filter']]
+    logger.info(
+        "Filtering orders by status: %s", config["order_status_filter"]
+    )
+    df_orders_delivered: DataFrame = df_orders[
+        df_orders["order_status"] == config["order_status_filter"]
+    ]
 
     # Select the columns order_id, order_purchase_timestamp
     logger.info("Selecting relevant columns...")
@@ -155,58 +165,74 @@ def main(args: argparse.Namespace) -> None:
 
     # Join df_order_items and df_orders by order_id
     logger.info("Joining dataframes...")
-    df_products_sales: DataFrame = \
-        df_order_items.join(df_orders_delivered, on="order_id", how="inner")
+    df_products_sales: DataFrame = df_order_items.join(
+        df_orders_delivered, on="order_id", how="inner"
+    )
 
     # Number of orders per product per week
     logger.info("Calculating number of orders per product per week...")
-    df_products_sales_weekly: DataFrame = \
+    df_products_sales_weekly: DataFrame = (
         calculate_orders_per_product_per_week(df_products_sales)
+    )
 
     # Save df_products_sales_weekly as parquet partitioned by product_id
-    logger.info("Saving results to %s...", config['output_path'])
+    logger.info("Saving results to %s...", config["output_path"])
     df_products_sales_weekly.to_parquet(
-        config['output_path'],
-        partition_cols=config['partition_cols'],
-        engine='fastparquet',
-        index=False)
+        config["output_path"],
+        partition_cols=config["partition_cols"],
+        engine="fastparquet",
+        index=False,
+    )
 
     logger.info("Pipeline execution completed successfully.")
 
 
 if __name__ == "__main__":
-    parser: argparse.ArgumentParser = \
-        argparse.ArgumentParser(description="Process data and save results.")
-    parser.add_argument("--config",
-                        type=str,
-                        default="pipeline_config.yaml",
-                        help="Path to YAML configuration file")
-    parser.add_argument("--orders_dataset_path",
-                        type=str,
-                        help="Path to the orders dataset CSV file")
-    parser.add_argument("--order_items_dataset_path",
-                        type=str,
-                        help="Path to the order items dataset CSV file")
-    parser.add_argument("--output_path",
-                        type=str,
-                        help="Path to save the output Parquet file")
-    parser.add_argument("--order_status_filter",
-                        type=str,
-                        help="Filter orders by status")
+    parser: argparse.ArgumentParser = argparse.ArgumentParser(
+        description="Process data and save results."
+    )
+    parser.add_argument(
+        "--config",
+        type=str,
+        default="pipeline_config.yaml",
+        help="Path to YAML configuration file",
+    )
+    parser.add_argument(
+        "--orders_dataset_path",
+        type=str,
+        help="Path to the orders dataset CSV file",
+    )
+    parser.add_argument(
+        "--order_items_dataset_path",
+        type=str,
+        help="Path to the order items dataset CSV file",
+    )
+    parser.add_argument(
+        "--output_path", type=str, help="Path to save the output Parquet file"
+    )
+    parser.add_argument(
+        "--order_status_filter", type=str, help="Filter orders by status"
+    )
     parser.add_argument(
         "--output_engine",
         type=str,
-        help="Engine to use for writing the output Parquet file")
-    parser.add_argument("--partition_cols",
-                        nargs='+',
-                        help="Columns to partition the output Parquet file")
+        help="Engine to use for writing the output Parquet file",
+    )
+    parser.add_argument(
+        "--partition_cols",
+        nargs="+",
+        help="Columns to partition the output Parquet file",
+    )
     parser.add_argument(
         "--log_level",
         type=str,
-        help="Logging level (DEBUG, INFO, WARNING, ERROR, CRITICAL)")
-    parser.add_argument("--log_file_path",
-                        type=str,
-                        default="pipeline.log",
-                        help="Path to the log file")
+        help="Logging level (DEBUG, INFO, WARNING, ERROR, CRITICAL)",
+    )
+    parser.add_argument(
+        "--log_file_path",
+        type=str,
+        default="pipeline.log",
+        help="Path to the log file",
+    )
     args: argparse.Namespace = parser.parse_args()
     main(args)
